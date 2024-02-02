d49957530d71b9805eabc04e31c7e78df492bfd8
Sebastian Elisa Reineke <elisanoemi.reineke@dataport.de>
2024-02-02 15:30:28 +0100

    remove example connector

--- src/mitarbeiterdb/Main.java
+++ src/mitarbeiterdb/Main.java
@@ -7,14 +7,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// Test Database connection
-		var dbTest = TestConnection.getInstance();
-		try {
-			dbTest.readDataBase();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
 		var db = Connector.getInstance();
 		var sql = new SQLBuilder();
 		try {
@@ -26,6 +18,7 @@ public class Main {
 		try {
 			db.sendSQLQuery("SELECT * FROM personen;");
 			db.sendSQLQuery("SELECT * FROM standorte;");
+			db.sendSQLQuery("SHOW TABLES");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
