9b284164a48ccf6d0fd7480706fcb1846ae072b7
Sebastian Elisa Reineke <elisanoemi.reineke@dataport.de>
2024-02-15 17:24:51 +0100

    move GetStandortID stuff to InputPanelFactory

--- src/mitarbeiterdb/implementation/controller/InputPanelFactory.java
+++ src/mitarbeiterdb/implementation/controller/InputPanelFactory.java
@@ -1,5 +1,11 @@
 package mitarbeiterdb.implementation.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import mitarbeiterdb.implementation.model.Connector;
+import mitarbeiterdb.implementation.model.SQLBuilder;
 import mitarbeiterdb.implementation.view.windows.subcomponents.InputPanel;
 import mitarbeiterdb.implementation.view.windows.subcomponents.PersonenInputPanel;
 import mitarbeiterdb.implementation.view.windows.subcomponents.StandorteInputPanel;
@@ -7,7 +13,19 @@ import mitarbeiterdb.implementation.view.windows.subcomponents.StandorteInputPan
 public class InputPanelFactory {
 	public InputPanel createPanel(TableType tableType) {
 		if (tableType == TableType.PERSONEN) {
-			return new PersonenInputPanel();
+			var sql = new SQLBuilder().getStandortIDs();
+			var standortIDs = new ArrayList<String>();
+			try {
+				List<List<String>> result = Connector.getInstance().sendSQLQuery(sql);
+				for (List<String> subList : result) {
+					standortIDs.add(subList.get(0));
+				}
+				standortIDs.set(0, ""); // replace column name with empty String (= standort not specified")
+
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return new PersonenInputPanel(standortIDs);
 		}
 		if (tableType == TableType.STANDORTE) {
 			return new StandorteInputPanel();
